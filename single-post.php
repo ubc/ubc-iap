dd5c35f2ee2e33ef1dbf70576c2afe87a2625227
Richard Tape <richard.tape@ubc.ca>
2021-08-31 09:50:05 -0700

    Added singular post template based on single page

---
+++ single-post.php
@@ -0,0 +1,25 @@
+<?php get_header(); ?>
+
+<div id="bootstrap_iso">
+	<?php
+		echo get_template_part( 
+			'partials/section',
+			'header',
+			[
+				'image'   => get_the_post_thumbnail_url(),
+				'title'   => get_the_title(),
+				'heading' => carbon_get_the_post_meta( 'crb_header_heading' ),
+			]
+		);
+	?>
+
+	<section class="bg-beige">
+		<div class="container pb-5">
+			<?php echo the_content(); ?>
+		</div>
+	</section>
+
+	<?php echo get_template_part('partials/pre-footer'); ?>
+</div>
+
+<?php get_footer(); // Loads the footer.php template. ?>
