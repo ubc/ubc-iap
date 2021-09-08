26a09db8bf38f6b999d4b653815f8dda03f99341
Kevin Slagboom <kslagboom@coolnewmedia.com>
2021-09-08 16:17:08 -0700

    Adding a stub template for the action plan plan

---
+++ template-action-plan.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Template Name: President's Message
+ */
+get_header(); // Loads the header.php template.
+?>
+
+<div id="bootstrap_iso">
+  
+  <?php //action plan code goes here. ?>
+
+  <?php echo get_template_part('partials/pre-footer') ?>
+</div>
+
+<?php
+get_footer(); // Loads the footer.php template. ?>
