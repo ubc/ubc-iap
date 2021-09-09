5923a7b7b7c909bfc584eff8615faf8499c046b4
Jon Donais <jon.donais@animikii.com>
2021-09-09 19:50:19 -0400

    Add goal cards to the action plan page.

--- partials/section-goals.php
+++ partials/section-goals.php
@@ -27,6 +27,214 @@
 
       ?>
 
+      <?php
+        for ($i = 0; $i < 1; $i++) {
+      ?>
+
+          <style>
+            .goal-card {
+              display: block;
+              width: 100%;
+              padding-top: 100%;
+              position: relative;
+              margin-bottom: 1em;
+            }
+
+            .goal-card-layer {
+              position: absolute;
+              top: 0;
+              left: 0;
+              width: 100%;
+              height: 100%;
+            }
+
+            .goal-card-background {
+              background: #002145;
+              background-size: cover;
+            }
+
+            .goal-card-overlay {
+              background: #002145;
+              mix-blend-mode: hard-light;
+              transition: opacity 300ms;
+            }
+
+            .goal-card-details {
+              color: white;
+              font-size: 32px;
+              padding: 32px;
+              
+              display: flex;
+              flex-direction: column;
+              align-items: center;
+              transition: opacity 300ms;
+            }
+
+            .goal-number-large {
+              display: flex;
+              justify-content: center;
+              align-items: center;
+              transition: opacity 300ms;
+            }
+
+            .goal-number-large p {
+              font-size: 60px!important;
+              color: #481700!important;
+              background-color: #DACAB5;
+              padding: 0px 26px;
+              border-radius: 100%;
+              border: 4px solid white;
+              box-shadow: 0px 4px 10px -2px black;
+            }
+
+            @media(min-width: 1140px) {
+              .goal-card-details, .goal-card-overlay {
+                opacity: 0;
+              }
+              .goal-card:hover .goal-card-details,
+              .goal-card:hover .goal-card-overlay,
+              .goal-card:focus .goal-card-details,
+              .goal-card:focus .goal-card-overlay {
+                opacity: 1;
+              }
+
+              .goal-card:hover .goal-number-large,
+              .goal-card:focus .goal-number-large {
+                opacity: 0;
+              }
+
+            }
+
+            .goal-number {
+              border-radius: 100%;
+              border: 2px solid white;
+              padding: 12px 14px;
+            }
+
+            .goal-text {
+              flex: 1;
+              display: flex;
+              flex-direction: column;
+              justify-content: space-evenly; 
+            }
+
+            .goal-text p {
+              color: white!important;
+              text-align: center;
+            }
+
+            .goal-header {
+              font-size: 18px!important;
+              margin-bottom: 0!important;
+            }
+
+            .goal-intro-text {
+              font-size: 16px!important;
+            }
+
+            @media(max-width: 1140px) {
+              .goal-number {
+                font-size: 40px!important;
+                font-weight: bold;
+                padding: 18px 20px;
+                border: 4px solid white;
+              }
+              .goal-header {
+                font-size: 30px!important;
+              }
+              .goal-intro-text {
+                font-size: 24px!important;
+              }
+            }
+            @media(max-width: 992px) {
+              .goal-number {
+                font-size: 25px!important;
+                font-weight: bold;
+                padding: 12px 16px;
+                border: 4px solid white;
+                margin-bottom: 8px;
+              }
+
+              .goal-header {
+                font-size: 20px!important;
+              }
+              .goal-intro-text {
+                font-size: 18px!important;
+              }
+            }
+
+            @media(max-width: 768px) {
+              .goal-number {
+                font-size: 40px!important;
+                font-weight: bold;
+                padding: 18px 20px;
+                border: 4px solid white;
+                margin-top: 8px;
+              }
+              .goal-header {
+                font-size: 34px!important;
+              }
+              .goal-intro-text {
+                font-size: 28px!important;
+              }
+            }
+ 
+            @media(max-width: 576px) {
+              .goal-card {
+                padding-top: 0;
+              }
+
+              .goal-card-details {
+                position: relative;
+              }
+
+              .goal-text {
+                justify-content: center;
+              }
+
+              .goal-number {
+                font-size: 40px!important;
+                font-weight: bold;
+                padding: 18px 20px;
+                border: 4px solid white;
+                margin-top: 8px;
+              }
+              .goal-header {
+                font-size: 25px!important;
+                margin-bottom: 12px!important;
+              }
+              .goal-intro-text {
+                font-size: 20px!important;
+              }
+            }
+ 
+
+          </style>
+  
+          <div class="col-12 col-md-6 col-xl-3">
+            <a class="goal-card" href="<?php echo get_permalink(get_the_ID()) ?>">
+                <div class='goal-card-background goal-card-layer' style='background-image: url(<?php echo get_the_post_thumbnail_url(get_the_ID()) ?>)'>
+                  <div class='goal-card-overlay goal-card-layer'></div>
+                </div>
+
+                <div class='goal-number-large goal-card-layer d-none d-xl-flex'>
+                  <p><?php echo wp_kses_post(carbon_get_the_post_meta('crb_goal_number')); ?></p>
+                </div>
+
+                <div class='goal-card-details goal-card-layer'>
+                  <div class='goal-number d-xl-none'><?php echo wp_kses_post(carbon_get_the_post_meta('crb_goal_number')); ?></div>
+
+                  <div class='goal-text'>
+                    <p class='goal-header'><strong><?php echo wp_kses_post(carbon_get_the_post_meta('crb_goal_intro_bold_text')); ?>:</strong></p>
+                    <p class='goal-intro-text'><?php echo wp_kses_post(carbon_get_the_post_meta('crb_goal_intro_regular_text')); ?></p>
+                  </div>
+
+                </div>
+
+            </a>
+          </div>
+
+<!--
           <div class="col-12 col-md-6 col-xl-3">
             <a href="<?php the_permalink() ?>">
               GOAL CARD
@@ -40,6 +248,8 @@
               </span>
             </a>
           </div>
+-->
+       <?php } ?>
 
       <?php
         }
--- template-action-plan.php
+++ template-action-plan.php
@@ -9,6 +9,10 @@ get_header(); // Loads the header.php template.
   .goals-sub-header {
     color: #4C4D4F!important;
   } 
+
+  .goals-header-image {
+    height: 600px;
+  }
 </style>
 
 <div id="bootstrap_iso">
@@ -31,45 +35,15 @@ get_header(); // Loads the header.php template.
       <h3 class='goals-sub-header'><?php echo wp_kses_post(carbon_get_the_post_meta('crb_goals_sub_heading')) ?></h3>
     </div>
 
-  </section>
-  <section class="bg-beige">
-    <div class="clip-ellipse">
-      <div class="bg-image py-3 py-md-5 py-lg-6 py-xl-10"style="background-image:url(<?php echo esc_url(wp_get_attachment_image_url(carbon_get_the_post_meta('crb_bubble_background_image'), 'full')) ?>">
-        <div class="container">
-        <?php echo get_template_part('partials/breadcrumbs', null, ['class' => 'white']) ?>
-          <div class="row justify-content-center">
-            <div class="col-12 mb-5 text-center">
-              <?php echo wpautop(wp_kses_post(carbon_get_the_post_meta('crb_section_1_content_1'))) ?>
-            </div>
-            <div class="col-md-6 col-lg-5 mb-4 mb-md-0 mx-lg-4 mx-xl-5">
-              <figure class="img-rounded--text burnt">
-                <div>
-                  <h2 class="mb-2 mb-md-3 text-burnt"><?php echo esc_html(carbon_get_the_post_meta('crb_section_1_bubble_1_heading')) ?></h2>
-                  <p><?php echo wp_kses_post(carbon_get_the_post_meta('crb_section_1_bubble_1_content')) ?></p>
-                </div>
-              </figure>
-            </div>
-            <div class="col-md-6 col-lg-5 mx-lg-5">
-              <figure class="img-rounded--text burnt">
-                <div>
-                  <h2 class="mb-2 mb-md-3 text-burnt"><?php echo esc_html(carbon_get_the_post_meta('crb_section_1_bubble_2_heading')) ?></h2>
-                  <p><?php echo wp_kses_post(carbon_get_the_post_meta('crb_section_1_bubble_2_content')) ?></p>
-                </div>
-              </figure>
-            </div>
-            <div class="col-12 mt-5 text-white">
-              <h2><?php echo esc_html(carbon_get_the_post_meta('crb_section_1_heading')) ?></h2>
-              <div class="columns-2 text-white">
-                <?php echo wpautop(wp_kses_post(carbon_get_the_post_meta('crb_section_1_content_2'))) ?>
-              </div>
-              <hr/>
-              <?php echo wpautop(wp_kses_post(carbon_get_the_post_meta('crb_section_1_content_3'))) ?>
-            </div>
-          </div>
-        </div>
-      </div>
+    <div class="bg-image bg-image--header goals-header-image <?php echo isset($args['white-background']) && $args['white-background'] ? 'white' : ''; ?> <?php echo isset($args['class']) ? $args['class'] : '' ?>" style="background-image:url(<?php echo esc_url(wp_get_attachment_image_url(carbon_get_the_post_meta('crb_goals_header_image'), 'header'),
+) ?>)">
     </div>
+
+
   </section>
+
+  <?php echo get_template_part('partials/section-goals') ?>
+
   <?php echo get_template_part('partials/pre-footer') ?>
 </div>
 
